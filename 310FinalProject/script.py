dfdaf00fa96086376553300e9144e342e2c5afb3
Cade Conner <55031759+realcadecon@users.noreply.github.com>
2020-11-16 15:46:28 -0600

    updatedScript

--- 310FinalProject/script.py
+++ 310FinalProject/script.py
@@ -1,7 +1,6 @@
 import csv
 print("hello")
-f = open("weatherDB.sql", "w+")
-f.write("""-- MySQL Administrator dump 1.4
+outputStr = """-- MySQL Administrator dump 1.4
     --
     -- ------------------------------------------------------
     -- Server version	5.1.38-community
@@ -86,17 +85,13 @@ CREATE TABLE fatality (
     PRIMARY KEY(EventID),
     FOREIGN KEY (EventID) REFERENCES storm(EventID)
 ) ENGINE=InnoDB CHARSET=utf8;
-""")
+"""
+with open('data_files/StormEvents_details_2020.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    outputStr+="NSERT INTO `adventureworks`.`address` VALUES  (1,'1970 Napa Ct.',NULL,'Bothell',79,'98011',0x0DCBAD9ACF363F4884D8585C2D4EC6E9,'1998-01-04 00:00:00'),"
+    for row in csv_reader:
+        outputStr +="";
 
-f.close();
-# with open('data_files/StormEvents_details_2020.csv') as csv_file:
-#     csv_reader = csv.reader(csv_file, delimiter=',')
-#     line_count = 0
-#     for row in csv_reader:
-#         if line_count == 0:
-#             print(f'Column names are {", ".join(row)}')
-#             line_count += 1
-#         else:
-#             print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-#             line_count += 1
-#     print(f'Processed {line_count} lines.')
+f = open("weatherDB.sql", "w+")
+f.write(outputStr)
+f.close()
